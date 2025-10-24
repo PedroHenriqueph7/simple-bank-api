5f3cd5553ff880d9d7ecf70ccc47e3f87cea556b
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2025-10-24 16:55:29 -0300

    config: Configurar ambiente de desenvolvimento e estruturar pastas do projeto

---
+++ src/test/java/org/pedrodev/simple_bank_api/SimpleBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pedrodev.simple_bank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
